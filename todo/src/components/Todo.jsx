9d8c7f99ba4a3e37b0a6d9c7991b9a89d954f8e9
David Quiñones <daquinons@users.noreply.github.com>
2019-06-18 15:29:27 +0200

    Add checkbox and display Todo

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
 const Todo = props => {
+  const toggleTodo = () => {
+    props.toggleTodo(props.id);
+  }
+
   return (
     <div>
       <p>
+        <CheckBox isChecked={props.completed} handleChecked={toggleTodo}/>
         {props.value}
       </p>
     </div>
   );
+};
+
+class CheckBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isChecked: this.props.isChecked };
+    this.handleChecked = this.handleChecked.bind(this); // set this, because you need get methods from CheckBox
+  }
+
+  handleChecked() {
+    this.setState({ isChecked: !this.state.isChecked });
+    this.props.handleChecked();
+  }
+
+  render() {
+    return (
+      <>
+        <input type="checkbox" onChange={this.handleChecked} />
+      </>
+    );
+  }
 }
 
 export default Todo;
